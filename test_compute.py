6c9a45cdfe5379cdccc8196abac70e25399650ca
RakicMladen <32570016+RakicMladen@users.noreply.github.com>
2018-02-23 11:22:34 +0100

    init commit

---
+++ test_compute.py
@@ -0,0 +1,10 @@
+from compute import divide
+from compute import multiply
+
+def test_divide():
+    x = divide(1,2)
+    assert x == 0.5
+
+def test_multiply():
+    x = multiply(2,2)
+    assert x == 4    
